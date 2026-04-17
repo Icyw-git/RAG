2da02c03fde1699d2e1b4188bf51448fccb1a6aa
icy_w <icy_wcz@sjtu.edu.cn>
2026-04-17 11:18:47 +0800

    add implementation of top10 retrieval script to query and save results

---
+++ top10.py
@@ -0,0 +1,42 @@
+import json
+from VectorBase import VectorStore
+from Embeddings import OpenAIEmbedding
+
+questions=[]
+with open('eval.jsonl', 'r', encoding='utf-8') as f:
+    for line in f:
+        data = json.loads(line)
+        new_question={
+            "id": data["id"],
+            "question":data['question']
+        }
+        questions.append(new_question)
+
+print(questions)
+
+vector=VectorStore() # 创建一个新的VectorStore对象
+embedding=OpenAIEmbedding()
+vector.load_vector('./storage') # 加载本地的数据库
+
+k=10
+top10=[]
+for i in questions:
+
+    source,chunk_id,content,scores = vector.query(i['question'], EmbeddingModel=embedding, k=k)
+    top10.append({
+        "id": i['id'],
+        "question": i['question'],
+        "top10": [
+            {
+                "source": source[j],
+                "chunk_id": chunk_id[j],
+                "content": content[j],
+                "score": scores[j]
+            }
+            for j in range(k)
+        ]
+    })
+
+with open("eval_top10.jsonl","w",encoding="utf-8") as f:
+    for row in top10:
+        f.write(json.dumps(row, ensure_ascii=False) + "\n")
